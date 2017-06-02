f969137600514ce93aec78844d4fbd692ab020fe
Dave SRF <master4491@gmail.com>
2017-06-01 22:42:39 -0500

    Add files via upload

--- start.py
+++ start.py
@@ -40,18 +40,18 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 YELLOW = (255, 255, 255)
 
+global FONT
+
 FONT = pygame.font.SysFont("Arial", 26)
 ButtonFont = pygame.font.SysFont("Arial", 12)
 
 is_running = True
-
+pygame.display.set_caption('Ratscape')
 CLOCK = pygame.time.Clock()
 
 # - - - - - - Definitions - - - - -
 
 def DrawHUD():
-    pygame.display.set_caption('Ratscape')
-
     SCREEN.fill(WHITE)
 
     #Hover effect on the positional keys
@@ -82,6 +82,7 @@ def DrawHUD():
         pygame.draw.rect(SCREEN, GRAY, (1100, 450, 50, 50))
         pygame.draw.rect(SCREEN, GRAY, (1200, 450, 50, 50))
 
+    pygame.draw.rect(SCREEN, BLACK, (40, 570, 1200, 120))
 
     #Drawing the Labels
     UpButtonText = ButtonFont.render("UP", 1, (0, 0, 0))
@@ -94,6 +95,10 @@ def DrawHUD():
     GoldLabel = FONT.render("Gold: ", 16, BLACK)
     GoldAmount = FONT.render(str(gold), 1024, BLACK)
 
+    Label = FONT.render(TextLine1, 1, WHITE)
+    Label2 = FONT.render(TextLine2, 1, WHITE)
+    Label3 = FONT.render(TextLine3, 1, WHITE)
+
     SCREEN.blit(UpButtonText, (1167, 420))
     SCREEN.blit(DownButtonText, (1155, 520))
     SCREEN.blit(LeftButtonText, (1110, 470))
@@ -104,17 +109,11 @@ def DrawHUD():
     SCREEN.blit(MPBar, (400, 540))
     SCREEN.blit(HealthBar, (40, 540))
 
-    #Drawing the main text box
-    pygame.draw.rect(SCREEN, BLACK, (40, 570, 1200, 120))
-
-    Label = FONT.render(TextLine1, 1, (255, 255, 255))
     SCREEN.blit(Label, (45, 575))
-    Label2 = FONT.render(TextLine2, 1, (255, 255, 255))
     SCREEN.blit(Label2, (45, 605))
-    Label3 = FONT.render(TextLine3, 1, (255, 255, 255))
     SCREEN.blit(Label3, (45, 635))
 
-    ScreenCords = FONT.render(("( " + str(CordX) + " , " + str(CordY) + " ) "), 1, (0, 0, 0))
+    ScreenCords = FONT.render(("( " + str(CordX) + "," + str(CordY) + " )"), 1, (0, 0, 0))
     SCREEN.blit(ScreenCords, (1000, 540))
 
     #Drawing the HP Bar
@@ -129,29 +128,15 @@ def DrawHUD():
 
     pygame.display.update()
 
-# Thanks Anthony for fixing the text bugs!
+# Sorry Anthony, the bugs are back :(
 
 def HealthCheck():
     if Health <= 0:
         TextLine1 = "Game Over"
-        Label = FONT.render(TextLine1, 1, (255, 255, 255))
-        SCREEN.blit(Label, (45, 575))
-
 
 def RoomCheck():
-    if CordY == 1 and CordX == 1:
-        TextLine1 = "Give Succ"
-        TextLine2 = " "
-        TextLine3 = " "
-
-    if CordY != 1 or CordX != 1:
-        TextLine1 = "Welcome to Ratscape, You are currently in the starting room."
-        TextLine2 = "Use the controls off to the right hand side to move to different rooms."
-        TextLine3 = "As you progress you will aquire treasure and fight monsters"
-
-def RedrawScreen():
-    SCREEN.fill(WHITE)
-    DrawHUD()
+    if CordX == 1 and CordY == 1:
+        TextLine1 = "Oh Yeah"
 
 #  - - - - - - GAME LOOP - - - - -
 
@@ -177,8 +162,8 @@ while is_running:
             pygame.draw.rect(SCREEN, HOVER_GRAY, (1200, 450, 50, 50))
             CordX = CordX + 1
 
-    RedrawScreen()
     RoomCheck()
+    DrawHUD()
     HealthCheck()
 
     CLOCK.tick(60)
